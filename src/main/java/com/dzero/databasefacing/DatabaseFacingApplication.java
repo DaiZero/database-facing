6e8edb987d9b6ae9da3f5cae98bbafa6023c5812
DaiZero <dzd624175217@gmail.com>
2020-03-09 21:35:05 +0800

    init

---
+++ src/main/java/com/dzero/databasefacing/DatabaseFacingApplication.java
@@ -0,0 +1,13 @@
+package com.dzero.databasefacing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseFacingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseFacingApplication.class, args);
+    }
+
+}
